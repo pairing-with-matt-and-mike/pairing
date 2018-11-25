73113e7540b66be67f655ffb93b9e5fe0f30d137
Matt Lee <thatismatt@gmail.com>
2018-11-25 00:01:44 +0000

    Single user.

--- pairing.sh
+++ pairing.sh
@@ -4,19 +4,24 @@
 # m5.xlarge
 
 function add-user {
-    USERNAME=$1
-    GH_USERNAME=$2
-    sudo adduser --disabled-password --gecos "" $USERNAME
-    wget https://github.com/$GH_USERNAME.keys
-    sudo mkdir /home/$USERNAME/.ssh/
-    sudo mv $GH_USERNAME.keys /home/$USERNAME/.ssh/authorized_keys
-    sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
-    sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys
+    GH_USERNAME=$1
+    mkdir -p ~/.ssh
+    wget https://github.com/$GH_USERNAME.keys -qO- >> ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
 }
 
-add-user matt thatismatt
-add-user mike mwilliamson
-add-user lauri liwp
+add-user thatismatt
+add-user mwilliamson
+add-user liwp
 
 sudo apt -y update
 sudo apt -y install emacs-nox
+git clone https://github.com/syl20bnr/spacemacs.git .emacs.d
+git clone https://github.com/pairing-with-matt-and-mike/dotfiles.git
+ln -s ~/dotfiles/.spacemacs ~/.spacemacs
+
+touch /tmp/pairing
+echo "tmux -S /tmp/pairing" > start.sh
+chmod +x start.sh
+echo "tmux -S /tmp/pairing attach" > join.sh
+chmod +x join.sh
