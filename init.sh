3f72d092aad5ee5cf42b2c98a5e62ad3f5b4d3d7
Matt Lee <thatismatt@gmail.com>
2019-03-02 23:13:13 +0000

    Update and rename dotfiles/pairing.sh to init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# git clone https://github.com/pairing-with-matt-and-mike/dotfiles.git
-# ./dotfiles/dotfiles/pairing.sh
+# git clone https://github.com/pairing-with-matt-and-mike/pairing.git
+# ./pairing/init.sh
 # emacs # init spacemacs
 
 # https://aws.amazon.com/ec2/pricing/on-demand/
 # m5.xlarge
+# m5.large
 
 function add-user {
     GH_USERNAME=$1
@@ -21,7 +22,8 @@ add-user liwp
 sudo apt -y update
 sudo apt -y install emacs-nox
 git clone https://github.com/syl20bnr/spacemacs.git .emacs.d
-ln -s ~/dotfiles/dotfiles/.spacemacs ~/.spacemacs
+ln -s ~/pairing/dotfiles/.spacemacs ~/.spacemacs
+ln -s ~/pairing/dotfiles/.tmux.conf ~/.tmux.conf
 
 touch /tmp/pairing
 echo "tmux -S /tmp/pairing" > start.sh
