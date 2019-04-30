dfadaf5664d6874502bb99fd2619935f79201d8a
Matt Lee <thatismatt@gmail.com>
2019-04-30 22:15:58 +0000

    Add persistence to kev.

--- kev/main.c
+++ kev/main.c
@@ -13,6 +13,23 @@ typedef struct List {
   struct List* tail;
 } List;
 
+typedef struct PutPayload {
+  char* key;
+  char* value;
+} PutPayload;
+
+typedef enum CommandType {
+  PUT
+} CommandType;
+
+typedef struct Command {
+  CommandType command_type;
+  union {
+    PutPayload put;
+    //GetPayload get;
+  } payload;
+} Command;
+
 char* lookup(List* store, char* key) {
   List* p = store;
   while (p != NULL) {
@@ -45,11 +62,76 @@ List* prepend(char* key, char* value, List* store) {
   return tmp;
 }
 
-int main () {
-
+List* init_store(FILE* data_file) {
   List* store = NULL;
 
-  size_t len = 10;
+  if (data_file == NULL) {
+    return store;
+  }
+
+  while (1) {
+    size_t key_len = 0;
+    char* key = NULL;
+    size_t value_len = 0;
+    char* value = NULL;
+    size_t bytes_read;
+
+    if ((bytes_read = getline(&key, &key_len, data_file)) == -1) {
+      free(key);
+      break;
+    }
+    key[bytes_read - 1] = '\0';
+    if ((bytes_read = getline(&value, &value_len, data_file)) == -1) {
+      free(key);
+      free(value);
+      break;
+    }
+    value[bytes_read - 1] = '\0';
+    if (value[0] == '\0') {
+      free(value);
+      value = NULL;
+    }
+    store = prepend(key, value, store);
+  }
+
+  return store;
+}
+
+Command create_put(char* key, char* value) {
+  Command command = {
+    .command_type = PUT,
+    .payload.put = { .key = key, .value = value }
+  };
+  return command;
+}
+
+List* update(Command command, List* store) {
+  return prepend(command.payload.put.key, command.payload.put.value, store);
+}
+
+void write_command(Command command, FILE* data_file) {
+  if (data_file == NULL) {
+    return;
+  }
+
+  switch (command.command_type) {
+  case PUT:
+    fputs(command.payload.put.key, data_file);
+    fputc('\n', data_file);
+    if (command.payload.put.value != NULL) {
+      fputs(command.payload.put.value, data_file);
+    }
+    fputc('\n', data_file);
+    break;
+  }
+}
+
+int main (int argc, char** argv) {
+  FILE* data_file = argc >= 2 ? fopen(argv[1], "a+") : NULL;
+
+  List* store = init_store(data_file);
+
+  size_t len = 1 << 10;
   char* line = (char*) malloc(len);
 
   setbuf(stdout, NULL);
@@ -78,36 +160,40 @@ int main () {
       continue;
     }
 
-    if (strncmp(line, "DEL ", 4) == 0) {
+    {
       char* key = NULL;
-      sscanf(line, "DEL %ms\n", &key);
-      store = prepend(key, NULL, store);
-      printf("OK\n");
-      continue;
+      if (sscanf(line, "DEL %ms\n", &key) == 1) {
+        Command command = create_put(key, NULL);
+        write_command(command, data_file);
+        store = update(command, store);
+        printf("OK\n");
+        continue;
+      }
     }
 
-    if (strncmp(line, "PUT ", 4) == 0) {
+    {
       char* key = NULL;
       char* value = NULL;
-      sscanf(line, "PUT %ms %ms\n", &key, &value);
-      store = prepend(key, value, store);
-      printf("OK\n");
-      continue;
+      if (sscanf(line, "PUT %ms %ms\n", &key, &value) == 2) {
+        Command command = create_put(key, value);
+        write_command(command, data_file);
+        store = update(command, store);
+        printf("OK\n");
+        continue;
+      }
     }
 
-    if (strncmp(line, "GET ", 4) == 0) {
+    {
       char* key = NULL;
-      sscanf(line, "GET %ms\n", &key);
+      if (sscanf(line, "GET %ms\n", &key) == 1) {
+        char* value = lookup(store, key);
 
-      char* value = lookup(store, key);
-
-      if (value != NULL) {
-        printf("%s\n", value);
-        printf("OK\n");
-      } else {
-        printf("NOT OK\n");
+        if (value != NULL) {
+          printf("%s\n", value);
+          printf("OK\n");
+          continue;
+        }
       }
-      continue;
     }
 
     if (strncmp(line, "QUIT\n", len) == 0) {
--- kev/tests.py
+++ kev/tests.py
@@ -5,7 +5,7 @@ import subprocess
 
 def test_quit():
     kev = Kev()
-    assert kev.command("QUIT") == [b"So long and thanks for all the fish.\n"]
+    assert kev.command("QUIT") == [b"So long and thanks for all the fish."]
     exit_code = kev.wait()
     assert exit_code == 0
 
@@ -19,24 +19,55 @@ def test_keys_request_on_empty_log_prints_ok():
 def test_put_prints_ok():
     kev = Kev()
     assert kev.command("PUT foo bar") == []
-    assert kev.command("GET foo") == [b"bar\n"]
+    assert kev.command("GET foo") == [b"bar"]
     assert kev.command("PUT foo baz") == []
-    assert kev.command("GET foo") == [b"baz\n"]
+    assert kev.command("GET foo") == [b"baz"]
     kev.command("QUIT")
     kev.wait()
-
 
 def test_can_get_keys_other_than_most_recent():
     kev = Kev()
     assert kev.command("PUT foo bar") == []
     assert kev.command("PUT baz quux") == []
-    assert kev.command("GET foo") == [b"bar\n"]
+    assert kev.command("GET foo") == [b"bar"]
     kev.command("QUIT")
     kev.wait()
 
 def test_returns_not_ok_for_non_existent_key():
     kev = Kev()
-    assert kev.command("GET foo") == [b"NOT OK\n"]
+    assert kev.command("GET foo") == [b"NOT OK"]
+    kev.command("QUIT")
+    kev.wait()
+
+@pytest.mark.parametrize("command", (
+    "GET ",
+    "GET",
+))
+def test_get_command_without_key_returns_not_ok(command):
+    kev = Kev()
+    assert kev.command(command) == [b"NOT OK"]
+    kev.command("QUIT")
+    kev.wait()
+
+@pytest.mark.parametrize("command", (
+    "PUT ",
+    "PUT k ",
+    "PUT k",
+    "PUT",
+))
+def test_put_command_with_missing_arguments_returns_not_ok(command):
+    kev = Kev()
+    assert kev.command(command) == [b"NOT OK"]
+    assert kev.command("KEYS") == []
+    kev.command("QUIT")
+    kev.wait()
+
+def test_del_command_without_key_returns_not_ok():
+    kev = Kev()
+    assert kev.command("PUT foo bar") == []
+    assert kev.command("DEL") == [b"NOT OK"]
+    assert kev.command("DEL ") == [b"NOT OK"]
+    assert kev.command("GET foo") == [b"bar"]
     kev.command("QUIT")
     kev.wait()
 
@@ -44,13 +75,13 @@ def test_deleted_items_return_null():
     kev = Kev()
     kev.command("PUT foo bar")
     kev.command("DEL foo")
-    assert kev.command("GET foo") == [b"NOT OK\n"]
+    assert kev.command("GET foo") == [b"NOT OK"]
     kev.command("QUIT")
     kev.wait()
 
 def test_non_existent_command():
     kev = Kev()
-    assert kev.command("RUBBISH") == [b"NOT OK\n"]
+    assert kev.command("RUBBISH") == [b"NOT OK"]
     kev.command("QUIT")
     kev.wait()
 
@@ -58,7 +89,7 @@ def test_keys_lists_them_all():
     kev = Kev()
     assert kev.command("PUT foo bar") == []
     assert kev.command("PUT baz quux") == []
-    assert kev.command("KEYS") == [b"foo\n", b"baz\n"]
+    assert kev.command("KEYS") == [b"foo", b"baz"]
     kev.command("QUIT")
     kev.wait()
 
@@ -68,13 +99,45 @@ def test_keys_lists_them_all_once():
     assert kev.command("PUT foo bar") == []
     assert kev.command("PUT baz quux") == []
     assert kev.command("DEL baz") == []
-    assert kev.command("KEYS") == [b"foo\n"]
+    assert kev.command("KEYS") == [b"foo"]
+    kev.command("QUIT")
+    kev.wait()
+
+def test_persistence(tmp_path):
+    data_path = str(tmp_path / "data.kev")
+    kev = Kev(data_path)
+    assert kev.command("PUT foo bar") == []
+    kev.command("QUIT")
+    kev.wait()
+    kev = Kev(data_path)
+    assert kev.command("GET foo") == [b"bar"]
+    kev.command("QUIT")
+    kev.wait()
+
+def test_persistence_del(tmp_path):
+    data_path = str(tmp_path / "data.kev")
+    kev = Kev(data_path)
+    assert kev.command("PUT foo bar") == []
+    kev.command("QUIT")
+    kev.wait()
+    kev = Kev(data_path)
+    assert kev.command("DEL foo") == []
+    kev.command("QUIT")
+    kev.wait()
+    kev = Kev(data_path)
+    assert kev.command("GET foo") == [b"NOT OK"]
     kev.command("QUIT")
     kev.wait()
 
 class Kev(object):
-    def __init__(self):
-        self._process = subprocess.Popen(["./kev"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
+    def __init__(self, data_path=None):
+        command = ["./kev"]
+        if data_path is not None:
+            command.append(data_path)
+        self._process = subprocess.Popen(command,
+                                         stdin=subprocess.PIPE,
+                                         stdout=subprocess.PIPE,
+                                         bufsize=1)
 
     def __read_until_ok(self):
         while True:
@@ -91,7 +154,7 @@ class Kev(object):
     def command(self, value):
         self._process.stdin.write(value.encode("utf-8") + b"\n")
         self._process.stdin.flush()
-        return list(self.__read_until_ok())
+        return [x.rstrip(b'\n') for x in self.__read_until_ok()]
 
     def stdout_line(self):
         return self._process.stdout.readline()
