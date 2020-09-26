206b6afb374365e87304011629583063ae2cfb06
Matt Lee <thatismatt@gmail.com>
2020-09-26 21:41:45 +0000

    Interactive control points

--- conifour/src/App.js
+++ conifour/src/App.js
@@ -1,3 +1,4 @@
+import { throttle } from "lodash";
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
@@ -27,24 +28,43 @@ function App() {
   const height = 200;
 
   const [state, setState] = useState({
+    start: { x: width / 4, y: (3 * height) / 4 },
     middle: { x: width / 2, y: height / 2 },
+    end: { x: (3 * width) / 4, y: (3 * height) / 4 },
   });
+
+  const onMoveRef = useRef(null);
+
+  const backgroundCanvasRef = useRef(null);
+  if (backgroundCanvasRef.current === null) {
+    const backgroundCanvas = document.createElement("canvas");
+    backgroundCanvas.width = width;
+    backgroundCanvas.height = height;
+    backgroundCanvasRef.current = backgroundCanvas;
+
+    const c = backgroundCanvas.getContext("2d");
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        c.fillStyle = hslToString(coordinatesToColour({ x, y }));
+        c.fillRect(x, y, 1, 1);
+      }
+    }
+  }
 
   const positions = interpolatePositions(state);
   const colours = positions.map(coordinatesToColour);
 
   function draw(element) {
     const c = element.getContext("2d");
-    for (let x = 0; x < element.width; x++) {
-      for (let y = 0; y < element.height; y++) {
-        c.fillStyle = hslToString(coordinatesToColour({ x, y }));
-        c.fillRect(x, y, 1, 1);
-      }
-    }
+    c.drawImage(backgroundCanvasRef.current, 0, 0);
 
     c.strokeStyle = "black";
     for (const position of positions) {
       drawCross(c, position);
+    }
+
+    for (const pt of [state.start, state.middle, state.end]) {
+      drawControlPoint(c, pt);
     }
   }
 
@@ -62,6 +82,14 @@ function App() {
     );
   }
 
+  const controlPointRadius = 5;
+
+  function drawControlPoint(c, { x, y }) {
+    c.beginPath();
+    c.arc(x, y, controlPointRadius, 0, 2 * Math.PI);
+    c.stroke();
+  }
+
   function drawLine(c, { x: x1, y: y1 }, { x: x2, y: y2 }) {
     c.beginPath();
     c.moveTo(x1, y1);
@@ -76,11 +104,29 @@ function App() {
   }
 
   function handleMouseDown({ x, y }) {
-    setState((state) => ({ ...state, start: { x, y } }));
+    for (const key of ["start", "middle", "end"]) {
+      const dx = state[key].x - x;
+      const dy = state[key].y - y;
+
+      if (dx ** 2 + dy ** 2 <= controlPointRadius ** 2) {
+        onMoveRef.current = throttle((value) => {
+          setState((state) => ({ ...state, [key]: value }));
+        }, 15);
+      }
+    }
+  }
+
+  function handleMouseMove({ x, y }) {
+    if (onMoveRef.current) {
+      onMoveRef.current({ x, y });
+    }
   }
 
   function handleMouseUp({ x, y }) {
-    setState((state) => ({ ...state, end: { x, y } }));
+    if (onMoveRef.current) {
+      onMoveRef.current({ x, y });
+      onMoveRef.current = null;
+    }
   }
 
   return (
@@ -90,6 +136,7 @@ function App() {
         width={width}
         draw={draw}
         onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
       />
       <Palette colours={colours} />
@@ -98,11 +145,9 @@ function App() {
 }
 
 function Canvas(props) {
-  const { height, width, draw, onMouseDown, onMouseUp } = props;
+  const { height, width, draw, onMouseDown, onMouseMove, onMouseUp } = props;
 
   const canvasRef = useRef();
-
-  const [isMoving, setIsMoving] = useState(false);
 
   useEffect(() => {
     if (canvasRef.current != null) {
@@ -119,17 +164,13 @@ function Canvas(props) {
 
   function handleMouseDown(event) {
     onMouseDown(calculateOffset(event));
-    setIsMoving(true);
   }
 
   function handleMouseMove(event) {
-    if (isMoving) {
-      onMouseUp(calculateOffset(event));
-    }
+    onMouseMove(calculateOffset(event));
   }
 
   function handleMouseUp(event) {
-    setIsMoving(false);
     onMouseUp(calculateOffset(event));
   }
 
