c0c1779e4ac9f5a2a344ba426a6997ae5b57fe20
Matt Lee <thatismatt@gmail.com>
2021-11-03 22:52:18 +0000

    More pagnem, amaze map, such wow

--- pagnem/pagnem.js
+++ pagnem/pagnem.js
@@ -7,10 +7,27 @@ var svgSelection = d3.select("svg");
 //   .attr("r", 25)
 //   .style("fill", "purple");
 
+let seed = d3.randomUniform()();
+let preseed = window.location.hash.substring(1);
+if (preseed != "random") {
+  if (preseed) {
+    seed = parseFloat(preseed);
+  } else {
+    window.location.hash = `#${seed}`;
+  }
+}
+
+const source = d3.randomLcg(seed);
+const randomInt = d3.randomInt.source(source);
+
+console.log("seed", seed);
+
+console.log(randomInt(0, 10)());
+
 const width = 100;
 const height = 100;
 
-const padding = 4;
+const padding = 1;
 const squareWidth = 10;
 
 const grid = repeat(() => repeat(() => "room", height), width);
@@ -49,10 +66,100 @@ function countNeighbours(x, y, type) {
 generate();
 
 async function generate() {
+  splitRoomVertically({
+    minX: 0,
+    maxX: width,
+    minY: 0,
+    maxY: height,
+    depth: 0,
+  });
+}
+
+async function splitRoomVertically({ minX, maxX, minY, maxY, depth }) {
+  if (maxX - minX < 3) {
+    return;
+  }
+
+  await sleep(10);
+
+  let wallX = randomInt(minX + 1, maxX - 1)();
+
+  for (let y = minY; y < maxY; y++) {
+    grid[wallX][y] = "wall";
+  }
+
+  grid[wallX][randomInt(minY, maxY)()] = "door";
+
+  view.render();
+
+  if (depth < 5) {
+    const ap = (
+      0 === randomInt(0, 4)() ? splitRoomVertically : splitRoomHorizontally
+    )({
+      minX,
+      maxX: wallX,
+      minY,
+      maxY,
+      depth: depth + 1,
+    });
+    const bp = (
+      0 === randomInt(0, 4)() ? splitRoomVertically : splitRoomHorizontally
+    )({
+      minX: wallX + 1,
+      maxX,
+      minY,
+      maxY,
+      depth: depth + 1,
+    });
+    await Promise.all([ap, bp]);
+  }
+}
+
+async function splitRoomHorizontally({ minX, maxX, minY, maxY, depth }) {
+  if (maxY - minY < 3) {
+    return;
+  }
+
+  await sleep(10);
+
+  let wallY = randomInt(minY + 1, maxY - 1)();
+
+  for (let x = minX; x < maxX; x++) {
+    grid[x][wallY] = "wall";
+  }
+
+  grid[randomInt(minX, maxX)()][wallY] = "door";
+
+  view.render();
+
+  if (depth < 5) {
+    const ap = (
+      0 === randomInt(0, 4)() ? splitRoomHorizontally : splitRoomVertically
+    )({
+      minX,
+      maxX,
+      minY,
+      maxY: wallY,
+      depth: depth + 1,
+    });
+    const bp = (
+      0 === randomInt(0, 4)() ? splitRoomHorizontally : splitRoomVertically
+    )({
+      minX,
+      maxX,
+      minY: wallY + 1,
+      maxY,
+      depth: depth + 1,
+    });
+    await Promise.all([ap, bp]);
+  }
+}
+
+async function generateWalk() {
   let startTime = Date.now();
   let visited = new Set();
-  let x = d3.randomInt(0, width)();
-  let y = d3.randomInt(0, height)();
+  let x = randomInt(0, width)();
+  let y = randomInt(0, height)();
   let n;
   for (n = 0; n < height; n++) {
     visited.add(x + "_" + y);
@@ -117,11 +224,15 @@ function squareToColor(square) {
       return "grey";
     case "corridor":
       return "red";
+    case "door":
+      return "grey";
+    case "wall":
+      return "purple";
   }
 }
 
 function randomChoose(array) {
-  return array[d3.randomInt(0, array.length)()];
+  return array[randomInt(0, array.length)()];
 }
 
 function repeat(value, times) {
