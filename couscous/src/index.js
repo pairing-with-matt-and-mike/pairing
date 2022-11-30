41f2a3486fbb57245aa8d4d3b8acd95f1727ad0b
Matt Lee <thatismatt@gmail.com>
2022-11-30 22:45:21 +0000

    More couscous!

--- couscous/src/index.js
+++ couscous/src/index.js
@@ -28,6 +28,12 @@ class Vec {
         this.x *= s;
         this.y *= s;
     }
+
+    setMagnitude(m) {
+        const currentMagnitude = (this.x ** 2 + this.y ** 2) ** 0.5;
+        const scaleFactor = m / currentMagnitude;
+        this.scale(scaleFactor);
+    }
 }
 
 class Point {
@@ -43,11 +49,21 @@ class Point {
     }
 
     move(acceleration) {
-        const v = this.pos.clone();
-        v.sub(this.prevPos);
-        v.add(acceleration);
+        const dt = 1;
+        const friction = 0;
+
+        const dtdt = dt * dt;
+
+        const xA = acceleration.x;
+        const xC = this.pos.x;
+        const xT = (2 - friction) * xC - (1 - friction) * this.prevPos.x + xA * dtdt;
+
+        const yA = acceleration.y;
+        const yC = this.pos.y;
+        const yT = (2 - friction) * yC - (1 - friction) * this.prevPos.y + yA * dtdt;
+
         this.prevPos = this.pos.clone();
-        this.pos.add(v);
+        this.pos = new Vec(xT, yT);
         this.pos.x = clamp(0, this.pos.x, width);
         this.pos.y = clamp(0, this.pos.y, height);
     }
@@ -94,12 +110,13 @@ class Constraint {
 
 class Blob {
     constructor(centre, radius) {
-        const freedom = 0.01;
+        const freedom = 0.009;
         const bigConstraintLength = 2 * Math.cos(22.5 / 180 * Math.PI) * radius;
 
         this.hullPoints = new Array(8).fill(0).map((_, i) => i * (2*Math.PI/8)).map((angle) => Point.fromPolar(centre, angle, radius));
 
         this.centerPoint = new Point(centre.x, centre.y);
+        this.radius = radius;
         this.points = [...this.hullPoints, this.centerPoint];
         this.constraints = [
             ...this.hullPoints.map((point, i) => {
@@ -115,6 +132,22 @@ class Blob {
         ];
     }
 
+    repel(blob) {
+        const pos = this.centerPoint.pos.clone();
+        pos.sub(blob.centerPoint.pos);
+
+        const minDistance = this.radius + blob.radius;
+        const currentDistance = (pos.x ** 2 + pos.y ** 2) ** 0.5;
+        const distanceToMove = (minDistance - currentDistance) / 2;
+
+        if (currentDistance < minDistance) {
+            const offset = pos.clone();
+            offset.setMagnitude(distanceToMove);
+            this.centerPoint.pos.add(offset);
+            blob.centerPoint.pos.sub(offset);
+        }
+    }
+
     draw() {
         noFill();
         strokeWeight(1);
@@ -125,6 +158,8 @@ class Blob {
                   c.pos.x, c.pos.y,
                   d.pos.x, d.pos.y);
         }
+
+        // render supports
         stroke(0, 0, 150);
         for (let i = 0; i < this.constraints.length; i++) {
             const { p, q } = this.constraints[i];
@@ -148,6 +183,15 @@ class Blob {
     move(acceleration) {
         this.points.forEach((p) => p.move(acceleration));
     }
+
+    spawn() {
+        const offsets = [new Vec(20, 10), new Vec(-20, -10)];
+        return offsets.map(offset => {
+            const centre = offset;
+            centre.add(this.centerPoint.pos);
+            return new Blob(centre, this.radius / 1.1);
+        })
+    }
 }
 
 function* slidingTuples(xs, width = 2) {
@@ -160,22 +204,47 @@ function* slidingTuples(xs, width = 2) {
     }
 }
 
-let blob = new Blob(new Vec(50, 50), 35);
+let blobs = [new Blob(new Vec(150, 50), 35)];
 let gravity = new Vec(0, 0.01);
+
+let slider;
 
 const height = 400;
 const width = 400;
 
 function setup() {
+    slider = createSlider(-0.1, 0.1, 0.01, 0.001);
+    slider.position(0, 0);
     createCanvas(width, height);
+    frameRate(200);
 }
 
 function draw() {
+    gravity = new Vec(0, slider.value());
     background(220);
-    blob.draw();
-    blob.constrain();
-    blob.move(gravity);
+
+    blobs.forEach((b1, i1) => {
+        blobs.forEach((b2, i2) => {
+            if (i1 === i2) return;
+
+            b1.repel(b2);
+        })
+    })
+
+    blobs.forEach((b) => {
+        b.draw();
+        b.constrain();
+        b.move(gravity);
+    });
 }
 
-window.setup = setup;
+function keyTyped() {
+    const [mother, child] = blobs.shift().spawn();
+    mother.move(new Vec(0.1, 0));
+    child.move(new Vec(-0.1, 0));
+    blobs.push(mother, child);
+}
+
 window.draw = draw;
+window.keyTyped = keyTyped;
+window.setup = setup;
